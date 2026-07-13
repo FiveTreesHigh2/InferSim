f39ee4844671c2acf10928852fffb90eca86ae3c
森年 <sennian.ybs@alibaba-inc.com>
2026-07-13 11:34:54 +0800

    test: Add pro5000 test cases

--- example/qwen3.5-35B-A3B/decode.sh
+++ example/qwen3.5-35B-A3B/decode.sh
@@ -6,3 +6,14 @@ python3 main.py --config-path hf_configs/qwen3.5-35B-A3B_config.json  \
   --decode-bs 224 \
   --use-fp8-gemm \
   --decode-only
+
+python3 main.py \
+  --config-path hf_configs/qwen3.5-35B-A3B_config.json \
+  --device-type PRO5000 \
+  --world-size 1 \
+  --tp-size 1 \
+  --use-fp8-gemm \
+  --decode-bs 224 \
+  --target-isl 4096 \
+  --target-osl 2048 \
+  --decode-only
--- example/qwen3.5-35B-A3B/prefill.sh
+++ example/qwen3.5-35B-A3B/prefill.sh
@@ -5,3 +5,14 @@ python3 main.py --config-path hf_configs/qwen3.5-35B-A3B_config.json \
   --use-fp8-gemm \
   --max-prefill-tokens 4096 \
   --prefill-only
+
+python3 main.py \
+  --config-path hf_configs/qwen3.5-35B-A3B_config.json \
+  --device-type PRO5000 \
+  --world-size 1 \
+  --tp-size 1 \
+  --use-fp8-gemm \
+  --max-prefill-tokens 4096 \
+  --target-isl 4096 \
+  --target-osl 2048 \
+  --prefill-only
